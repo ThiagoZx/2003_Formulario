730b88176899fbcc33a2f175ee90bf6497a94157
ThiagoZx <TheThiagoo0@gmail.com>
2015-03-30 11:19:58 -0300

    Formulário de Cadastro
    Formulário a ser preenchido com informações pessoais do usuário, que
    serão salvas em um arquivo de texto

---
+++ Formulário/Formulário/Class1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Formulário
+{
+    class Funcionario
+    {
+        //Vericação de todas as informações
+        private bool allSet = true;
+        public bool getAllSet() {
+            return allSet;
+        }
+
+        //Informações pessoais do usuário
+        public string name;
+        private int age;
+        private string job;
+        private string salary;
+        private string email;
+        private string tel;
+        private string blood;
+        private int filho;
+        private string estCiv;
+        private string CEP;
+        private string ender;
+        private int num;
+        private string sex;
+
+        //Função de envio para o banco de dados
+        public string asString()
+        {
+            return name + "#" + age + "#" + job + "#" + salary + "#" + email + "#" + tel + "#"+ blood + "#" + filho + "#" + estCiv + "#" + CEP + "#" + ender + "#" + num + "#" + sex + "\r\n";
+        }
+
+        //Função para recuperação do banco de dados 
+        public void fromString(string data) 
+        {
+            string[] info = data.Split('#');
+            name = info[0];
+            age = Convert.ToInt32(info[1]);
+            job = info[2];
+            salary = info[3];
+            email = info[4];
+            tel = info[5];
+            blood = info[6];
+            filho = Convert.ToInt32(info[7]);
+            estCiv = info[8];
+            CEP = info[9];
+            ender = info[10];
+            num = Convert.ToInt32(info[11]);
+            sex = info[12];
+        }
+
+        //Função de verificação de dados e envio para a classe
+        public void setAll(string name, string age, string job, string salary, string email, string num,
+                           string tel, string blood, string filho, string estCiv, string CEP, string ender, string sex)
+        {
+            if (name == "") {
+                allSet = false;
+            } else {
+                this.name = name; 
+            }
+            
+            if (age == "") {
+                allSet = false;
+            } else {
+                this.age = Convert.ToInt32(age);
+            }
+
+            if (job == "") {
+                allSet = false;
+            } else {
+                this.job = job;
+            }
+
+            if (salary == "R$ ") {
+                allSet = false;
+            } else {
+                this.salary = salary;
+            }
+
+            if (email == "") {
+                allSet = false;
+            } else {
+                this.email = email;
+            }
+
+            if (num == "") {
+                allSet = false;
+            } else { 
+                this.num = Convert.ToInt32(num);
+            }
+
+            if ((tel.Length < 14) || (tel.Length > 15)) {
+                allSet = false;
+            } else {
+                this.tel = tel;
+            }
+
+            if (blood == "") {
+                allSet = false;
+            } else {
+                this.blood = blood;
+            }
+            
+            this.filho = Convert.ToInt32(filho);
+           
+            if (estCiv == "") {
+                allSet = false;
+            } else {
+                this.estCiv = estCiv;
+            }
+
+            if (CEP.Length < 9) {
+                allSet = false;
+            } else {
+                this.CEP = CEP;
+            }
+
+            if (ender.Length <= 4) {
+                allSet = false;
+            } else {
+                this.ender = ender;
+            }
+
+            if (sex == null) {
+                allSet = false;
+            } else {
+                this.sex = sex;
+            }
+        }
+
+        //Função para salvar informações no banco de dados
+        public void saveText() 
+        {
+            string save = asString();
+            System.IO.File.AppendAllText(@"WriteLines.txt", save);
+        }
+    }
+}
---
+++ Formulário/Formulário/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Formulário
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Funcionario> FuncList = new List<Funcionario>();
+        
+
+        //Checar sexo
+        string sex;
+
+        //Funções para alteração da imagem do cadastro
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Formulário.Properties.Resources.Icon_maleAvatar;
+            sex = "Masculino";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Formulário.Properties.Resources.Icon_femaleAvatar;
+            sex = "Feminino";
+        }
+
+        //Função de alteração do salário
+
+        private void textBox4_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Back) {
+                if (textBox4.TextLength <= 3) {
+                    textBox4.Text = "R$ ";
+                }
+            }
+        }
+
+        //Função de aviso de campos inválidos
+
+        private void validBox(string name, string age, string job, string salary, string email, string num, string tel, string blood, string filho, string estCiv, string CEP, string ender, string sex)
+        {
+            if (name != "")
+            {
+                label18.Text = "";
+            }
+            else
+            {
+                label18.Text = "*";
+                label30.Text = "* Campo Inválido";
+            }
+
+            if (age != "")
+            {
+                label20.Text = "";
+            }
+            else
+            {
+                label20.Text = "*";
+                label30.Text = "* Campo Inválido";
+            }
+
+            if (job != "")
+            {
+                label19.Text = "";
+            }
+            else
+            {
+                label19.Text = "*";
+                label30.Text = "* Campo Inválido";
+            }
+
+            if (salary != "R$ ")
+            {
+                label22.Text = "";
+            }
+            else
+            {
+                label22.Text = "*";
+                label30.Text = "* Campo Inválido";
+            }
+
+            if (email != "")
+            {
+                label21.Text = "";
+            }
+            else
+            {
+                label21.Text = "*";
+                label30.Text = "* Campo Inválido";
+            }
+
+            if (num != "")
+            {
+                label28.Text = "";
+            }
+            else
+            {
+                label28.Text = "*";
+                label30.Text = "* Campo Inválido";
+            }
+
+            if ((tel.Length >= 14) && (tel.Length <= 15))
+            {
+                label24.Text = "";
+            }
+            else
+            {
+                label24.Text = "*";
+                label30.Text = "* Campo Inválido";
+            }
+
+            if (blood != "")
+            {
+                label23.Text = "";
+            }
+            else
+            {
+                label23.Text = "*";
+                label30.Text = "* Campo Inválido";
+            }
+
+            if (estCiv != "")
+            {
+                label25.Text = "";
+            }
+            else
+            {
+                label25.Text = "*";
+                label30.Text = "* Campo Inválido";
+            }
+
+            if (CEP.Length == 9)
+            {
+                label29.Text = "";
+            }
+            else
+            {
+                label29.Text = "*";
+                label30.Text = "* Campo Inválido";
+            }
+
+            if (ender.Length >= 4)
+            {
+                label27.Text = "";
+            }
+            else
+            {
+                label27.Text = "*";
+                label30.Text = "* Campo Inválido";
+            }
+
+            if (sex != null)
+            {
+                label26.Text = "";
+            }
+            else
+            {
+                label26.Text = "*";
+                label30.Text = "* Campo Inválido";
+            }
+        }
+
+        //Função do Botão de adicionar
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Funcionario func = new Funcionario();
+            func.setAll(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox6.Text, textBox8.Text, textBox5.Text, comboBox1.Text, Convert.ToString(numericUpDown1.Value), comboBox2.Text, textBox9.Text, textBox7.Text, sex);
+            
+            if (func.getAllSet()) {
+                label30.Text = "";
+                FuncList.Add(func);
+                func.saveText();
+                listBox1.Items.Add(func.name);
+            } else { 
+                func = null;
+                MessageBox.Show("Alguns campos de entrada são inválidos!");
+                validBox(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox6.Text, textBox8.Text, textBox5.Text, comboBox1.Text, Convert.ToString(numericUpDown1.Value), comboBox2.Text, textBox9.Text, textBox7.Text, sex);
+            }
+        }
+    }
+}
